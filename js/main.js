c30c1303f831982c7cb60b809f2b3c79edd59d8e
Levi Arteaga PC personal <levi.arteaga@gmail.com>
2024-07-06 08:30:08 -0400

    primera configuracion de mi web

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const destinations = document.querySelectorAll('.destination');
 
+    destinations.forEach(destination => {
+        destination.addEventListener('click', function() {
+            alert('Coño esperate, mi mania y yo estamos todavia trabajando en ello.');
+        });
+    });
+});
