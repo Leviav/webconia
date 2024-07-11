38f31d208a08bda618070270d6919856a60b6f1a
Leviav <145229988+Leviav@users.noreply.github.com>
2024-07-11 10:33:55 +0000

    Añadir páginas de servicios y mejorar estilos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,10 @@
 // src/components/Footer.js
 export default function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <p>&copy; {new Date().getFullYear()} Trisqueltour. Todos los derechos reservados.</p>
       <style jsx>{`
-        footer {
-          background: #333;
-          color: white;
-          text-align: center;
-          padding: 1rem;
-          position: relative;
-          bottom: 0;
-          width: 100%;
-        }
+       
       `}</style>
     </footer>
   );
---
+++ src/pages/services/tour-guides.js
@@ -0,0 +1,13 @@
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+
+export default function TourGuides() {
+  return (
+    <div className = "container">
+      <Navbar />
+      <h1>Tours Guiados</h1>
+      <p>Detalles sobre nuestros tours guiados...</p>
+      <Footer />
+    </div>
+  );
+}
